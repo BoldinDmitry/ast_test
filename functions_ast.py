a3fd3c9a065a26e7e1b04223587227b8e5e5ba34
Stalin <dima1231231233@gmail.com>
2016-08-18 18:23:52 +0300

    1.2.4

--- functions_ast.py
+++ functions_ast.py
@@ -1,30 +1,5 @@
 import random
 from ast import *
-import codegen
-
-
-def term_res(result):
-    result_res = result
-    number_of_numbers = random.randint(2, 4)
-    print(number_of_numbers)
-    operations = ["+", "-", "*", "/"
-                                 ""]
-    term = []
-    result = 0
-    for i in range(number_of_numbers):
-        number = (random.randrange(-20, 20))
-        term.append(str(number))
-        function = random.choice(operations)
-        term.append(function)
-
-    term.pop(len(term) - 1)
-    # print(term, result)
-    term.insert(0, str(result))
-    term.insert(1, "-")
-    term = " ".join(term)
-    # print(term)
-    exec(str("result = " + term))
-    return term
 
 
 def variable(name, number):
@@ -45,22 +20,23 @@ def term_for_variable(value, term):
     return str(value) + " " + "=" + " " + term
 
 
+def term_res(result):
+    number_of_numbers = random.randint(2, 4)
+    print(number_of_numbers)
+    operations = ["+", "-", "*", "/"]
+    term = []
+    result = 0
+    for i in range(number_of_numbers):
+        number = (random.randrange(-20, 20))
+        term.append(str(number))
+        function = random.choice(operations)
+        term.append(function)
+    term.pop(len(term) - 1)
+    term.insert(0, str(result))
+    term.insert(1, "-")
+    term = " ".join(term)
+    exec(str("result = " + term))
+    return term
+
+
 operations_ex = [Add(), Sub(), Mult(), Div()]
-
-i = int(input("Введите колличество строчек: \n"))
-new_code = ""
-old_code = ""
-n = 0
-while n <= i:
-    one_line = random.choice([variable("a", random.randint(-20, 20)),
-                              operations(random.choice(operations_ex), random.choice(["a", random.randint(-20, 20)]),
-                                         random.choice(["a", random.randint(-20, 20)]))])
-    new_code += codegen.to_source(one_line) + '\n'
-    try:
-        exec(new_code)
-        old_code = new_code
-        n += 1
-    except:
-        new_code = old_code
-
-print(new_code)
---
+++ main.py
@@ -0,0 +1,2 @@
+import functions_ast
+
